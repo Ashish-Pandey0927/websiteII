1d2994045fdfe0af36052c4f7efd4962f0b797dd
kanchanp <kanchanp937@gmail.com>
2025-05-26 00:19:26 +0530

    fix font sizes

--- frontend/src/pages/ServicesPage/OfferedServices.jsx
+++ frontend/src/pages/ServicesPage/OfferedServices.jsx
@@ -1,98 +1,48 @@
-// import React from 'react';
-// import './OfferedServices.css';
-
-// const services = [
-//   {
-//     title: 'Mobile Development',
-//     description: 'Mobile apps offer a wide range of benefits to customers, enhancing their overall experience and providing convenience, accessibility, and value. Here are some key benefits that mobile apps bring to customers',
-//     icon: 'MobileDevelopment.png',
-//   },
-//   {
-//     title: 'Web Development',
-//     description: 'Web apps offer customers a flexible, accessible, and user-friendly platform for accessing services, information, and functionality over the internet. By leveraging the capabilities of web technologies.',
-//     icon: 'WebDevelopment.png',
-//   },
-//   {
-//     title: 'Cloud Services',
-//     description: 'Mobile apps offer a wide range of benefits to customers, enhancing their overall experience and providing convenience, accessibility, and value. Here are some key benefits that mobile apps bring to customers',
-//     icon: 'CloudServices.png',
-//   },
-//   {
-//     title: 'Quality Assurance',
-//     description: 'Web apps offer customers a flexible, accessible, and user-friendly platform for accessing services, information, and functionality over the internet. By leveraging the capabilities of web technologies.',
-//     icon: 'QualityAssurance.png',
-//   },
-//   {
-//     title: 'SalesForce Solution ',
-//     description: 'Mobile apps offer a wide range of benefits to customers, enhancing their overall experience and providing convenience, accessibility, and value. Here are some key benefits that mobile apps bring to customers',
-//     icon: 'SalesForceSolution.png',
-//   },
-//   {
-//     title: 'CI/CD Devops ',
-//     description: 'Web apps offer customers a flexible, accessible, and user-friendly platform for accessing services, information, and functionality over the internet. By leveraging the capabilities of web technologies.',
-//     icon: 'Devops.png',
-//   },
-// ];
-
-// const ServicesSection = () => {
-//   return (
-//     <section className="services-section">
-//       <div className='offered-services-header'>
-//       <h2 className="section-title">Services We Offer</h2>
-//       <p className="section-subtitle">
-//       We work together with our client to execute impactful product.
-//       </p>
-//       </div>
-//       <div className="services-grid">
-//         {services.map((service, idx) => (
-//           <div className="service-card" key={idx}>
-//             <div className="icon-wrapper">
-//               <div className="icon-bg" />
-//               <img src={service.icon} alt={service.title} className="service-icon" />
-//             </div>
-//             <h3 className="service-title">{service.title}</h3>
-//             <p className="service-desc">{service.description}</p>
-//           </div>
-//         ))}
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default ServicesSection;
 import React from "react";
 import "./OfferedServices.css";
+import MobileIcon from "../../assets/app-development.svg";
+import WebIcon from "../../assets/web-development.svg";
+import CloudIcon from "../../assets/cloud-computing.svg"
+import QAIcon from "../../assets/quality-assurance.svg"
+import SalesforceIcon from "../../assets/sales-force_17645757.svg"
+import DevOpsIcon from "../../assets/Devops.svg";
 
 const services = [
   {
     title: 'Mobile Development',
     description: 'Mobile apps offer a wide range of benefits to customers, enhancing their overall experience and providing convenience, accessibility, and value. Here are some key benefits that mobile apps bring to customers',
-    serviceIcon: 'MobileDevelopment.png',
+    serviceIcon: MobileIcon,
+    isSvg: false,
   },
   {
     title: 'Web Development',
     description: 'Web apps offer customers a flexible, accessible, and user-friendly platform for accessing services, information, and functionality over the internet. By leveraging the capabilities of web technologies.',
-    serviceIcon: 'WebDevelopment.png',
+    serviceIcon: WebIcon,
+    isSvg: false,
   },
   {
     title: 'Cloud Services',
     description: 'Mobile apps offer a wide range of benefits to customers, enhancing their overall experience and providing convenience, accessibility, and value. Here are some key benefits that mobile apps bring to customers',
-    serviceIcon: 'CloudServices.png',
+      serviceIcon: CloudIcon,
+    isSvg: false,
   },
   {
     title: 'Quality Assurance',
     description: 'Web apps offer customers a flexible, accessible, and user-friendly platform for accessing services, information, and functionality over the internet. By leveraging the capabilities of web technologies.',
-    serviceIcon: 'QualityAssurance.png',
+   serviceIcon: QAIcon,
+    isSvg: false,
   },
   {
     title: 'SalesForce Solution ',
     description: 'Mobile apps offer a wide range of benefits to customers, enhancing their overall experience and providing convenience, accessibility, and value. Here are some key benefits that mobile apps bring to customers',
-    serviceIcon: 'SalesForceSolution.png',
+   serviceIcon: SalesforceIcon,
+    isSvg: false,
   },
   {
     title: 'CI/CD Devops ',
     description: 'Web apps offer customers a flexible, accessible, and user-friendly platform for accessing services, information, and functionality over the internet. By leveraging the capabilities of web technologies.',
-    serviceIcon: 'Devops.png',
+  serviceIcon: DevOpsIcon,
+    isSvg: false,
   },
 ];
 
