1f4ec8ade1587ee2bbd35dee7b92a0f05772743f
kanchanp <197561203+kanchanp937@users.noreply.github.com>
2025-07-10 20:17:03 +0530

    added relevent emojis in services sub menu

--- frontend/src/components/newheader.menu.js
+++ frontend/src/components/newheader.menu.js
@@ -6,25 +6,24 @@ export const menuData = [
       {
         title: "Web Development",
         offeredServices: [
-         
-          { label: "Frontend Development", icon: "📝" },
-          { label: "Backend Development", icon: "📝" },
-          { label: "Salesforce Web Component", icon: "📝" },
-          { label: "Integration of Third-Party Services", icon: "📝" },
-          { label: "Deployment and Hosting", icon: "📝" },
-          { label: "Maintenance and Updates", icon: "📝" },
+          { label: "Frontend Development", icon: "🎨" }, // UI/design
+          { label: "Backend Development", icon: "🛠️" }, // server-side
+          { label: "Salesforce Web Component", icon: "⚡" }, // LWC
+          { label: "Integration of Third-Party Services", icon: "🔌" }, // APIs
+          { label: "Deployment and Hosting", icon: "🚀" }, // deployment
+          { label: "Maintenance and Updates", icon: "🧰" }, // support
         ],
         techStack: [
           { label: "React", icon: "⚛️" },
-          { label: "Vue", icon: "⚛️" },
-          { label: "Angular", icon: "⚛️" },
+          { label: "Vue", icon: "🖖" },
+          { label: "Angular", icon: "📐" },
           { label: "Node.js", icon: "🟩" },
-          { label: "Python", icon: "⚛️" },
-          { label: "Express.js", icon: "⚛️" },
-          { label: "Java", icon: "⚛️" },
-          { label: "PHP", icon: "⚛️" },
-          { label: "C#", icon: "⚛️" },
-          { label: "Next.js", icon: "⚛️" }
+          { label: "Python", icon: "🐍" },
+          { label: "Express.js", icon: "🚂" },
+          { label: "Java", icon: "☕" },
+          { label: "PHP", icon: "🐘" },
+          { label: "C#", icon: "🎯" },
+          { label: "Next.js", icon: "⏭️" }
         ]
       },
       {
@@ -32,18 +31,18 @@ export const menuData = [
         offeredServices: [
           { label: "Android Development", icon: "🤖" },
           { label: "iOS Development", icon: "📱" },
-          { label: "Flutter Development", icon: "🔄" },
-          { label: "React Native Development", icon: "🔄" },
-          { label: "Xamarin/MAUI Development", icon: "🔄" },
+          { label: "Flutter Development", icon: "💙" },
+          { label: "React Native Development", icon: "⚛️" },
+          { label: "Xamarin/MAUI Development", icon: "📦" },
           { label: "Mobile Apps Migration", icon: "🔄" }
         ],
         techStack: [
-          { label: "Kotlin", icon: "⚛️" },
-          { label: "Dart", icon: "💙" },
+          { label: "Kotlin", icon: "🅺" },
+          { label: "Dart", icon: "🎯" },
           { label: "Swift", icon: "🦅" },
-          { label: "MAUI/.NET Standard", icon: "🦅" },
-          { label: "Java", icon: "🦅" },
-          { label: "Objective-C", icon: "🦅" }
+          { label: "MAUI/.NET Standard", icon: "🌐" },
+          { label: "Java", icon: "☕" },
+          { label: "Objective-C", icon: "📘" }
         ]
       },
       {
@@ -52,30 +51,30 @@ export const menuData = [
           { label: "AWS Solutions", icon: "☁️" },
           { label: "Azure Integration", icon: "🔷" },
           { label: "GCP Services", icon: "🟦" },
-           { label: "Azure Services", icon: "🔷" },
+          { label: "Azure Services", icon: "🔷" },
         ],
         techStack: [
           { label: "AWS (Amazon Web Services)", icon: "☁️" },
-          { label: "Microsoft Azure", icon: "☁️" },
+          { label: "Microsoft Azure", icon: "🔷" },
           { label: "GCP ", icon: "🟦" },
-          { label: "IBM Cloud ", icon: "🔷" },
-          { label: "Oracle Cloud", icon: "🔷" },
-          { label: "Serverless Computing", icon: "🔷" },
-          { label: "Virtual Machines", icon: "🔷" },
-          { label: "Cloud Databases", icon: "🔷" },
-          { label: "Cloud Storage (S3, Blob)", icon: "🔷" },
-          { label: "Cloud Monitoring and Analytics", icon: "🔷" },
+          { label: "IBM Cloud ", icon: "🌩️" },
+          { label: "Oracle Cloud", icon: "🟥" },
+          { label: "Serverless Computing", icon: "🛰️" },
+          { label: "Virtual Machines", icon: "💻" },
+          { label: "Cloud Databases", icon: "🗄️" },
+          { label: "Cloud Storage (S3, Blob)", icon: "🧺" },
+          { label: "Cloud Monitoring and Analytics", icon: "📈" },
         ]
       },
       {
         title: "Quality Assurance",
         offeredServices: [
           { label: "Salesforce Testing", icon: "🧪" },
-          { label: "Cloud Services QA", icon: "🤖" },
-          { label: "Mobile Apps QA", icon: "⚡" },
-          { label: "Web Development QA", icon: "🧪" },
-          { label: "CI/CD and DevOps Testing", icon: "🤖" },
-          { label: "Performance and Security Testing", icon: "⚡" }
+          { label: "Cloud Services QA", icon: "☁️" },
+          { label: "Mobile Apps QA", icon: "📲" },
+          { label: "Web Development QA", icon: "🌐" },
+          { label: "CI/CD and DevOps Testing", icon: "🧬" },
+          { label: "Performance and Security Testing", icon: "🔒" }
         ],
         techStack: [
           { label: "Selenium", icon: "🧩" },
@@ -83,55 +82,55 @@ export const menuData = [
           { label: "TestNG", icon: "📬" },
           { label: "Postman", icon: "📬" },
           { label: "Jenkins", icon: "🧩" },
-          { label: "Appium", icon: "📊" },
-          { label: "Cypress", icon: "🧩" },
-          { label: "QTest", icon: "📊" },
-          { label: "Bugzilla", icon: "📬" },
-          { label: "JIRA", icon: "📬" },
+          { label: "Appium", icon: "📲" },
+          { label: "Cypress", icon: "🌲" },
+          { label: "QTest", icon: "🧪" },
+          { label: "Bugzilla", icon: "🐞" },
+          { label: "JIRA", icon: "📋" },
         ]
       },
       {
         title: "Salesforce",
         offeredServices: [
           { label: "Salesforce Development", icon: "🔧" },
-          { label: "Apex Programming", icon: "🔌" },
+          { label: "Apex Programming", icon: "📟" },
           { label: "Salesforce Lightning Web Components (LWC)", icon: "⚡" },
-          { label: "Salesforce Integration", icon: "🔌" },
-          { label: "Salesforce Custom Objects & Fields", icon: "🔌" },
-          { label: "Salesforce Data Migration", icon: "🔌" },
+          { label: "Salesforce Integration", icon: "🔗" },
+          { label: "Salesforce Custom Objects & Fields", icon: "🧩" },
+          { label: "Salesforce Data Migration", icon: "📤" },
         ],
         techStack: [
           { label: "LWC", icon: "💡" },
           { label: "Apex", icon: "🅰️" },
-          { label: "Salesforce APIs", icon: "💡" },
-          { label: "Salesforce Lightning Experience", icon: "🅰️" },
+          { label: "Salesforce APIs", icon: "🔌" },
+          { label: "Salesforce Lightning Experience", icon: "⚡" },
           { label: "Visualforce", icon: "👁️" },
-          { label: "Salesforce Integration", icon: "👁️" },
-          { label: "Salesforce Customization", icon: "👁️" },
-          { label: "Salesforce Mobile SDK", icon: "👁️" },
-          { label: "Salesforce Data Loader", icon: "👁️" },
-          { label: "Salesforce App Exchange Apps", icon: "👁️" },
+          { label: "Salesforce Integration", icon: "🔗" },
+          { label: "Salesforce Customization", icon: "⚙️" },
+          { label: "Salesforce Mobile SDK", icon: "📱" },
+          { label: "Salesforce Data Loader", icon: "📦" },
+          { label: "Salesforce App Exchange Apps", icon: "🏪" },
         ]
       },
       {
         title: "CI/CD DevOps",
         offeredServices: [
-          { label: "Cloud DevOps Services", icon: "🔗" },
+          { label: "Cloud DevOps Services", icon: "☁️" },
           { label: "AWS Devops Services", icon: "📈" },
-          { label: "GCP Devops Services", icon: "🤖" },
-          { label: "Azure Devops Services", icon: "🤖" }
+          { label: "GCP Devops Services", icon: "🛠️" },
+          { label: "Azure Devops Services", icon: "🔷" }
         ],
         techStack: [
           { label: "Jenkins", icon: "☕" },
-          { label: "GitLab CI/CD", icon: "🐙" },
-          { label: "CircleCI", icon: "🐳" },
-          { label: "Travis CI", icon: "☕" },
-          { label: "Azure DevOps", icon: "🐙" },
-          { label: "GitHub Actions", icon: "🐳" },
-          { label: "AWS CodePipeline", icon: "☕" },
-          { label: "Docker & Kubernetes", icon: "🐙" },
-          { label: "Terraform", icon: "🐳" },
-          { label: "Helm Charts", icon: "🐳" }
+          { label: "GitLab CI/CD", icon: "🦊" },
+          { label: "CircleCI", icon: "⭕" },
+          { label: "Travis CI", icon: "🛠️" },
+          { label: "Azure DevOps", icon: "🔷" },
+          { label: "GitHub Actions", icon: "🐙" },
+          { label: "AWS CodePipeline", icon: "🛤️" },
+          { label: "Docker & Kubernetes", icon: "🐳" },
+          { label: "Terraform", icon: "🌍" },
+          { label: "Helm Charts", icon: "📈" }
         ]
       }
     ]
