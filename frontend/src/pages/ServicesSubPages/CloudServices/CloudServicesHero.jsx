bf8203a7a7057f8675c9f51074eab003e56440bc
kanchanp <197561203+kanchanp937@users.noreply.github.com>
2025-07-15 14:21:16 +0530

    added hightlighter text to services sub pages

--- frontend/src/pages/ServicesSubPages/CloudServices/CloudServicesHero.jsx
+++ frontend/src/pages/ServicesSubPages/CloudServices/CloudServicesHero.jsx
@@ -10,6 +10,31 @@ function CloudServicesHero() {
         className="services-sub-pages-container cloud-services-hero">
         <DashboardHeader  activeLink={activeLink} setActiveLink={setActiveLink} />
           <div
+            className="service-sub-page-highlight-wrapper"
+          >
+            <div className="service-sub-page-vector-highlight">
+              <svg
+                width="26"
+                height="30"
+                viewBox="0 0 26 30"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M12.7904 0.352417V14.8524M12.7904 29.3524V14.8524M12.7904 14.8524L1.57091 7.74457M12.7904 14.8524L24.5709 21.9603M12.7904 14.8524L1.57091 21.9603M12.7904 14.8524L24.5709 7.74457"
+                  stroke="#A7F46A"
+                  strokeWidth="4"
+                />
+              </svg>
+            </div>
+
+            <div
+              className="service-sub-page-highlight-text"
+            >
+              Flexible, Secure Cloud Solutions for Growing Startups
+            </div>
+          </div>
+          <div
             className="services-sub-pages-hero-content">
             <h1 className="services-sub-pages-hero-heading" >
               Cloud <br/>Services
